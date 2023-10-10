4e59389102701f848ada6b95ed76c2db80afad20
Jendry Rodriguez <jimmirodriguez71@gmail.com>
2023-10-10 13:02:27 +0200

    test scraper now load every product in a page

--- scrape.py
+++ scrape.py
@@ -13,57 +13,74 @@ cookie_button.click()
 
 time.sleep(1)
 
-results = driver.find_elements(By.CLASS_NAME, 'product-category-overview_category__vqzcb')
+categories = driver.find_elements(By.CLASS_NAME, 'product-category-overview_category__vqzcb')
 
 product_types = []
 
-for result in results:
-    info = result.find_element(By.CLASS_NAME, 'taxonomy-card_imageLink__4b6bk')
-    img_element = result.find_element(By.CLASS_NAME, 'taxonomy-card_image__RPcqB')
+for category in categories:
+    info = category.find_element(By.CLASS_NAME, 'taxonomy-card_imageLink__4b6bk')
+    img_element = category.find_element(By.CLASS_NAME, 'taxonomy-card_image__RPcqB')
     type = info.get_attribute('title')
     link = info.get_attribute('href')
-    img_link = img_element.get_attribute('src')
-    product_types.append({'Type': type, 'link':link, 'Img': img_link})
+    img = img_element.get_attribute('src')
+    product_types.append({'Type': type, 'link':link, 'image': img})
 
-print(product_types)
-
-'''
-page = requests.get(URL+product_types[0]['link'])
-soup = BeautifulSoup(page.content, 'html.parser')
-
-results = soup.find_all("article", class_="product-card-portrait_root__ZiRpZ product-grid-lane_gridItems__BBa4h")
-
-products = []
-
-for result in results:
-    info = result.find("a", class_="link_root__EqRHd")
-    name = info['title']
-    link = info['href']
-    products.append({'name': name, 'link':link})
-
-
-driver.get(URL+products[4]['link'])
-
-
-cookie_button = driver.find_element(By.XPATH, '//*[@id="accept-cookies"]')
-
-cookie_button.click()
+categories[0].click()
 
 time.sleep(1)
 
-table = driver.find_element(By.XPATH,'//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table')
+def get_displayed_amount():
+    total_products_info = driver.find_element(By.XPATH, '//*[@id="start-of-content"]/div[3]/span').text
+    total_products_info = total_products_info.split()
+    current = int(total_products_info[0])
+    total= int(total_products_info[3])
+    return current, total
 
-body = table.find_element(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody')
+current_amount, total_amount = get_displayed_amount()
 
-rows = body.find_elements(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody/tr')
+more_button = driver.find_element(By.CLASS_NAME, 'button-default_primary__mURfJ')
 
-nutrition = []
+while(current_amount < total_amount):
+    print(current_amount)
+    time.sleep(1)
+    more_button.click()
+    more_button = driver.find_element(By.CLASS_NAME, 'button-default_primary__mURfJ')
+    current_amount, total_amount = get_displayed_amount()
 
-for index, row in enumerate(rows):
-    name = body.find_element(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody/tr['+str(index+1)+']/td[1]').text
-    macro = body.find_element(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody/tr['+str(index+1)+']/td[1]').text
-    nutrition.append({"name": name, "macro":macro})
+
+products = driver.find_elements(By.CLASS_NAME, 'product-card-portrait_root__ZiRpZ')
+
+product_list = []
+
+def convert_products():
+    for product in products:
+        info = product.find_element(By.CLASS_NAME, 'link_root__EqRHd')
+        img_element = product.find_element(By.TAG_NAME, 'img')
+        name = info.get_attribute('title')
+        link = info.get_attribute('href')
+        img = img_element.get_attribute('src')
+        product_list.append({'name': name, 'link':link, 'image':img})
 
 
-print(nutrition)
-'''
+convert_products()
+
+print(len(product_list))
+
+
+# driver.get(URL+products[4]['link'])
+
+# table = driver.find_element(By.XPATH,'//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table')
+
+# body = table.find_element(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody')
+
+# rows = body.find_elements(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody/tr')
+
+# nutrition = []
+
+# for index, row in enumerate(rows):
+#     name = body.find_element(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody/tr['+str(index+1)+']/td[1]').text
+#     macro = body.find_element(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody/tr['+str(index+1)+']/td[1]').text
+#     nutrition.append({"name": name, "macro":macro})
+
+
+# print(nutrition)
