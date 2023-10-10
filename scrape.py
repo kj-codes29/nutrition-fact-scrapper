6bf2945831264cbb729d7bb934738e7f334a75f5
Jendry Rodriguez <jimmirodriguez71@gmail.com>
2023-10-10 16:41:47 +0200

    scraper is able to extract all the nutrition information related to a product

--- scrape.py
+++ scrape.py
@@ -30,14 +30,45 @@ driver.implicitly_wait(1)
 
 product_list = []
 
-def convert_products(products):
-    for product in products:
-        info = product.find_element(By.CLASS_NAME, 'link_root__EqRHd')
-        img_element = product.find_element(By.TAG_NAME, 'img')
-        name = info.get_attribute('title')
-        link = info.get_attribute('href')
-        img = img_element.get_attribute('src')
-        product_list.append({'name': name, 'link':link, 'image':img})
+
+
+def convert_table_info():
+    table = driver.find_element(By.XPATH,'//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table')
+
+    body = table.find_element(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody')
+
+    rows = body.find_elements(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody/tr')
+
+    nutrition = []
+
+    for index, row in enumerate(rows):
+        name = body.find_element(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody/tr['+str(index+1)+']/td[1]').text
+        macro = body.find_element(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody/tr['+str(index+1)+']/td[2]').text
+        nutrition.append({"name": name, "macro":macro})
+    
+    return nutrition
+
+def convert_product(product):
+    info = product.find_element(By.CLASS_NAME, 'link_root__EqRHd')
+    img_element = product.find_element(By.TAG_NAME, 'img')
+    name = info.get_attribute('title')
+    link = info.get_attribute('href')
+    img = img_element.get_attribute('src')
+
+    product.click()
+
+    driver.implicitly_wait(1)
+
+    # nutrition facts
+    nutrition = convert_table_info()
+    product_list.append({'name': name, 'link':link, 'image':img, 'Nutrition Info': nutrition})
+
+    # go back in history
+    driver.back()
+    driver.implicitly_wait(1)
+        
+
+# print(nutrition)
 
 def get_displayed_amount():
     total_products_info = driver.find_element(By.XPATH, '//*[@id="start-of-content"]/div[3]/span').text
@@ -50,7 +81,7 @@ current_amount, total_amount = get_displayed_amount()
 
 products = driver.find_elements(By.CLASS_NAME, 'product-card-portrait_root__ZiRpZ')
 
-while(current_amount <= 111):
+while(current_amount <= 75):
     more_button = driver.find_element(By.CLASS_NAME, 'button-default_primary__mURfJ')
     more_button.click()
     print('......loading products')
@@ -63,24 +94,6 @@ while(current_amount <= 111):
     product_lane = driver.find_elements(By.CLASS_NAME, 'product-grid-lane_root__rii9N')[-2]
     new_products = product_lane.find_elements(By.CLASS_NAME, 'product-card-portrait_root__ZiRpZ')
     # get information about each product
-    convert_products(new_products)
+    convert_product(new_products[1])
 
-print(len(product_list))
-
-# driver.get(URL+products[4]['link'])
-
-# table = driver.find_element(By.XPATH,'//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table')
-
-# body = table.find_element(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody')
-
-# rows = body.find_elements(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody/tr')
-
-# nutrition = []
-
-# for index, row in enumerate(rows):
-#     name = body.find_element(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody/tr['+str(index+1)+']/td[1]').text
-#     macro = body.find_element(By.XPATH, '//*[@id="start-of-content"]/div[2]/div/div[1]/div[1]/div[3]/table/tbody/tr['+str(index+1)+']/td[1]').text
-#     nutrition.append({"name": name, "macro":macro})
-
-
-# print(nutrition)
+print(product_list)
